d3c6fc02264ea7ce464b20b1de9d98323b5ca88c
manuel <manuel.silva@embedded.ufcg.edu.br>
2018-04-27 17:51:24 -0300

    Task #14: Criar modelo de redação

---
+++ models/essay.model.js
@@ -0,0 +1,37 @@
+/*jshint esversion: 6 */
+/*global require, exports, module */
+/*eslint no-console: 0 */
+/* jshint strict: false */
+
+let mongoose = require('mongoose');
+
+let EssaySchema = new mongoose.Schema({
+
+    theme: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    type: {
+        type: String,
+        enum: ['Enem', 'Vestibular', 'Concurso'],
+        required: true,
+        trim: true
+    },
+
+    title: {
+        type: String,
+        trim: true
+    },
+
+    text: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+const Essay = mongoose.model('Essay', UserSchema);
+
+module.exports = Essay;
