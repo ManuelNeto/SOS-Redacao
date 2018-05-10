cdc7cd2b8db074b44d5ddb7c65e2c5c186477e49
RaquelRufino <raquel.paz@ccc.ufcg.edu.br>
2018-05-10 15:58:51 -0300

    make it better

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ if (env === 'development') {
     require('./config/database.js')('mongodb://localhost/SOS-REDACAO');
 } else {
     require('./config/database.js')('mongodb://sosredacao:projeto1_@ds119650.mlab.com:19650/sosredacao');
-   }
+}
 
 module.exports = app;
 
