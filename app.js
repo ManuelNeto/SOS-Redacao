0e28aa607b1e5eaf691dfbab8d09ef7c8c227851
RaquelRufino <raquel.paz@ccc.ufcg.edu.br>
2018-05-10 15:13:56 -0300

    make it better

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var app = require('./config/express')();
 
-require('./config/database.js')('mongodb://localhost/SOS-REDACAO');
+var env = process.argv[2] || process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    require('./config/database.js')('mongodb://localhost/SOS-REDACAO');
+} else {
+    require('./config/database.js')('mongodb://sosredacao:projeto1_@ds119650.mlab.com:19650/sosredacao');
+   }
 
 module.exports = app;
 
