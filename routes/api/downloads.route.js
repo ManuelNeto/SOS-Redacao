53279e67b39cad59e2b75098f7d4fba1c702ce81
Manuel Neto <manuel.neto@ccc.ufcg.edu.br>
2018-05-27 11:36:00 -0300

    Criar serviço para download de materiais de estudo

--- routes/api/downloads.route.js
+++ routes/api/downloads.route.js
@@ -7,8 +7,9 @@ let express = require('express');
 let router = express.Router();
 let tokenValidator = require('../../util/token.validator');
 let EssayController = require('../../controllers/essay.controller');
+let StudyMaterialController = require('../../controllers/studyMaterial.controller');
 
 router.get('/', EssayController.downloadEssayModel);
-
+router.post('/', StudyMaterialController.downloadStudyMaterial);
 
 module.exports = router;
