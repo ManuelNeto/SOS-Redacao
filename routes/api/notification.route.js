23f7f80b7277410d3a8050cabe63c891ddf577af
manuel <manuel.silva@embedded.ufcg.edu.br>
2018-06-29 10:36:53 -0300

    Criação do crud de notificações

---
+++ routes/api/notification.route.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 6 */
+/*global require, module */
+/*eslint no-console: 0 */
+/* jshint strict: false */
+
+let express = require('express');
+let router = express.Router();
+let tokenValidator = require('../../util/token.validator');
+let NotificationController = require('../../controllers/notification.controller');
+
+
+
+router.get('/:id', tokenValidator, NotificationController.getNotifications);
+router.post('/', tokenValidator, NotificationController.createNotification);
+router.delete('/:id', tokenValidator, NotificationController.deleteNotification);
+
+module.exports = router;
