20a48a21fcaefded5d5bfb43554000cf3de670da
manuel <manuel.silva@embedded.ufcg.edu.br>
2018-06-29 10:19:32 -0300

    Criação do modelo de notificação

---
+++ models/notifications.model.js
@@ -0,0 +1,49 @@
+/*jshint esversion: 6 */
+/*global require, exports, module */
+/*eslint no-console: 0 */
+/* jshint strict: false */
+
+let mongoose = require('mongoose');
+
+let NotificationSchema = new mongoose.Schema({
+
+    emitter: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+
+    },
+
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    text: {
+      type: String,
+      trim: true,
+      required: true
+    },
+
+    title: {
+      type: String,
+      trim: true,
+      required: true
+    },
+
+    type: {
+      type: String,
+      trim: true,
+      required: true
+    },
+
+    date: {
+      type: String,
+      trim: true,
+      required: true
+    }
+
+});
+
+const Notification = mongoose.model('Notification', NotificationSchema);
+
+module.exports = Notification;
